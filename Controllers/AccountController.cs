80f5e978222666681801b63bf6b238c95d704729
Edaware Patricia <eedawarez@gmail.com>
2020-03-07 16:34:52 +0100

    Add project files.

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using UserManagement.Models;
+
+namespace UserManagement.Controllers
+{
+    public class AccountController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+      
+        public ViewResult Register() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterModel registerModel)
+        {
+            RegisterModel register = new RegisterModel();
+            using (var httpClient = new HttpClient())
+            {
+                StringContent content = new StringContent(JsonConvert.SerializeObject(registerModel), Encoding.UTF8, "application/json");
+
+                using (var response = await httpClient.PostAsync("http://localhost:60628/Users/register", content))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    try
+                    {
+                        register = JsonConvert.DeserializeObject<RegisterModel>(apiResponse);
+                    }
+                    catch (Exception ex)
+                    {
+                        ViewBag.Result = apiResponse;
+                        return View();
+                    }
+                }
+            }
+            return View(register);
+        }
+
+
+        public ViewResult Login() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> Login(AuthenticateModel authenticateModel)
+        {
+            AuthenticateModel authenticate = new AuthenticateModel();
+            using (var httpClient = new HttpClient())
+            {
+                StringContent content = new StringContent(JsonConvert.SerializeObject(authenticateModel), Encoding.UTF8, "application/json");
+
+                using (var response = await httpClient.PostAsync("http://localhost:60628/Users/authenticate", content))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    try
+                    {
+                        authenticate = JsonConvert.DeserializeObject<AuthenticateModel>(apiResponse);
+                    }
+                    catch (Exception ex)
+                    {
+                        ViewBag.Result = apiResponse;
+                        return View();
+                    }
+                }
+            }
+            //return View(authenticate);
+            //return RedirectToAction("Index");
+            return RedirectToAction("Index", "Product", new { check = true});
+        }
+
+
+
+
+
+
+    }
+}
---
+++ Controllers/ProductController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using UserManagement.Models;
+
+namespace UserManagement.Controllers
+{
+    public class ProductController : Controller
+    {
+        public async Task<IActionResult> Index(bool check)
+        {
+            if (check)
+            {
+                List<Product> prods = new List<Product>();
+                using (var httpClient = new HttpClient())
+                {
+                    using (var response = await httpClient.GetAsync("https://localhost:44318/api/products"))
+                    {
+                        string apiResponse = await response.Content.ReadAsStringAsync();
+                        prods = JsonConvert.DeserializeObject<List<Product>>(apiResponse);
+                    }
+                }
+                return View(prods);
+            }
+            else
+            {
+                return RedirectToAction("Login", "Account");
+            }
+        }
+
+
+
+
+
+
+    }
+}
